824fd40a46c4a4b3f0cb9409655cd62a6b796982
Jonatan Olofsson <jonatan.olofsson@gmail.com>
2012-11-12 10:04:23 +0100

    System settings and types

---
+++ include/sys/settings.hpp
@@ -0,0 +1,15 @@
+#ifndef SYS_SETTINGS_HPP_
+#define SYS_SETTINGS_HPP_
+
+#include <os/types.hpp>
+
+namespace sys {
+    namespace settings {
+        using os::Scalar;
+
+        static const Scalar system_frequency    = 100; ///< System frequency, in Hz
+        static const Scalar dT                  = 1/system_frequency; ///< System steptime
+    }
+}
+
+#endif
---
+++ include/sys/types.hpp
@@ -0,0 +1,10 @@
+#ifndef SYS_TYPES_HPP_
+#define SYS_TYPES_HPP_
+
+#include <os/types.hpp>
+
+namespace sys {
+    typedef os::Scalar Scalar;
+}
+
+#endif
