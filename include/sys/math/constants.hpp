032f2aaaf908fac63e498e77c2e57b4300f94c93
Jonatan Olofsson <jonatan.olofsson@gmail.com>
2012-11-12 10:05:09 +0100

    Utility header for common mathematical constants

---
+++ include/sys/math/constants.hpp
@@ -0,0 +1,18 @@
+#ifndef SYS_MATH_CONSTANTS_HPP_
+#define SYS_MATH_CONSTANTS_HPP_
+
+#define USING_XYZ static const int X = 0; static const int Y = 1; static const int Z = 2;
+#define USING_XYZW USING_XYZ static const int W = 3;
+
+#define QUATERNION_ROTATION_FROM_ROTVEL(X,Y,Z) \
+      0 , -(Z),  (Y), (X), \
+     (Z),   0 ,  (X), (Y), \
+    -(Y),  (X),   0 , (Z), \
+    -(X), -(Y), -(Z),  0 ;
+
+namespace sys {
+    namespace math {
+        static const int EPSILON = 1e-16;
+    }
+}
+#endif
