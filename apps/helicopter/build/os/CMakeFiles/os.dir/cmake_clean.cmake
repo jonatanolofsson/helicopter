655c5d5949c989e32d2a7f99d7298019eff074ab
Jonatan Olofsson <jonatan.olofsson@gmail.com>
2013-06-24 00:38:22 +0200

    Removed build directory from version control

--- apps/helicopter/build/os/CMakeFiles/os.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-FILE(REMOVE_RECURSE
-  "libos.pdb"
-  "libos.a"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/os.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
