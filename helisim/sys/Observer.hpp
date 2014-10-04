0a933d49ecc2fbf87e1de97089d353e432be58fd
Jonatan Olofsson <jonatan.olofsson@gmail.com>
2014-10-04 19:31:21 +0200

    First step in observer refactor

--- helisim/sys/Observer.hpp
+++ helisim/sys/Observer.hpp
@@ -2,27 +2,23 @@
 #ifndef SYS_OBSERVER_API_HPP_
 #define SYS_OBSERVER_API_HPP_
 
-#include <sys/math/states.hpp>
 #include <sys/math/models.hpp>
 #include <sys/math/filtering.hpp>
 #include <os/clock.hpp>
 
 namespace sys {
     namespace observer {
-        typedef math::models::SHelicopter       StateDescription;
-        typedef math::models::C0                ControlDescription;
-        typedef math::models::A0                AuxiliaryDescription;
-        typedef math::models::Description<StateDescription, ControlDescription, AuxiliaryDescription> ModelDescription;
+        typedef math::models::SCart3DQuat<>                 StateDescription;
+        typedef math::models::ConstantVelocities6D<StateDescription> MotionModel;
 
         typedef math::EKF                                   Algorithm;
-        typedef math::GaussianFilter<ModelDescription>      Filter;
-        typedef math::models::Helicopter<ModelDescription>  MotionModel;
+        typedef math::GaussianFilter<StateDescription>      Filter;
         typedef os::SystemTime                              TriggerType;
 
-        typedef ModelDescription::States                    SystemState;
+        typedef StateDescription::StateVector               SystemState;
 
         namespace sensors {
-            typedef math::GaussianMeasurement<math::models::Gps<ModelDescription>> Gps;
+            typedef math::GaussianMeasurement<math::models::Gps<StateDescription>> Gps;
         }
     }
 }
