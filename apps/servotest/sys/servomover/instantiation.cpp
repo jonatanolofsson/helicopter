bff631c6c3ccf49c3c694602dee699c318aa94a9
Jonatan Olofsson <jonatan.olofsson@gmail.com>
2014-10-04 23:01:14 +0200

    Removed apps

--- apps/servotest/sys/servomover/instantiation.cpp
+++
@@ -1,8 +0,0 @@
-#include <sys/Servomover.hpp>
-#include <sys/servomover/implementation.hpp>
-
-namespace sys {
-    namespace servomover {
-        template class Servomover<TriggerType, SerialLink>;
-    }
-}
--- apps/servotest/sys/states/events.hpp
+++
@@ -1,5 +0,0 @@
-#pragma once
-#ifndef SYS_STATES_EVENTS_HPP_
-#define SYS_STATES_EVENTS_HPP_
-#include <sys/events.hpp>
-#endif
