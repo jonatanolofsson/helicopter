a9088fe04ffd0ec7ba5025900d459259a707b6e7
Jonatan Olofsson <jonatan.olofsson@gmail.com>
2014-10-05 08:25:21 +0200

    Remove stm messaging

--- com/StmInstantiation.cpp
+++
@@ -1,8 +0,0 @@
-#include <os/com/getSignal.hpp>
-#include <sys/com/Stm.hpp>
-
-INSTANTIATE_SIGNAL(sys::stm::SensorMessage);
-INSTANTIATE_SIGNAL(sys::stm::ControlMessage);
-INSTANTIATE_SIGNAL(sys::stm::IrCameraMessage);
-INSTANTIATE_SIGNAL(sys::stm::TowerMessage);
-INSTANTIATE_SIGNAL(sys::stm::IoctlMessage);
