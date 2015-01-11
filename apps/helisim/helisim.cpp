bd61149254e116d3ad1dc2637796f227d6149307
Jonatan Olofsson <jonatan.olofsson@gmail.com>
2015-01-11 18:57:42 +0100

    Add support for arguments/parameters and os initialization

--- apps/helisim/helisim.cpp
+++ apps/helisim/helisim.cpp
@@ -1,8 +1,10 @@
 #include <sys/StateMachine.hpp>
 #include <os/com/getSignal.hpp>
 #include <os/clock.hpp>
+#include <os/core/init.hpp>
 
-int main(int, char*[]) {
+int main(int argc, char** argv) {
+    os::init(argc, argv);
     sys::StateMachine::run();
 
     std::cout << "Exiting" << std::endl;
